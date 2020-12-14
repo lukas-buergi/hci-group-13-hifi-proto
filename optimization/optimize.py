70e96ee70ce4f235e122ba105ce1c72f95331f2b
Lukas Bürgi <lubuergi@student.ethz.ch>
2020-12-14 11:03:24 +0100

    cleaned up

--- optimization/optimize.py
+++ optimization/optimize.py
@@ -159,7 +159,7 @@ class Optimization():
         measurements = {}
         simulations = {}
         for measurement in self.measurementFiles:
-            with open(measurement, newline='') as csvfile:
+            with open("data/" + measurement, newline='') as csvfile:
                 reader = csv.reader(csvfile)
                 header = reader.__next__()
                 measurements[measurement] = []
@@ -269,10 +269,10 @@ def plotCost(files):
 if __name__ == "__main__":
     measurementFiles = ['flurinGatheredDataPage1-11endedAt21_07_24.csv', 'lukasJustReadingGatheredDataPage1-4endedAt18_08_42.csv', 'lukasJustReadingGatheredDataPage7-12endedAt18_14_56.csv', 'exampleData.csv', 'perfectArtificalTestData.csv']
 
-    if(True):
+    if(False):
        randomSearch(measurementFiles[0:3])
 
-    if(False):
+    if(True):
         opt = Optimization(measurementFiles[0:3])
         params = [0.045, 0.9, 0.3, 10] # pretty good
         #params = [0.2931978195602054, 0.436936186829908, 0.5129171007824062, 1000000]
